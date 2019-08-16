6316b7236a0e6860f81a180b93b005a78cbdfea3
phungvanquang <phungvanquang1997@gmail.com>
2019-08-16 17:24:27 +0700

    swap columns in db

--- dump_sql.sql
+++ dump_sql.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : java
-Source Server Version : 50723
+Source Server         : admin
+Source Server Version : 50724
 Source Host           : localhost:3306
 Source Database       : java
 
 Target Server Type    : MYSQL
-Target Server Version : 50723
+Target Server Version : 50724
 File Encoding         : 65001
 
-Date: 2019-08-15 20:13:53
+Date: 2019-08-16 17:24:03
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,17 +39,14 @@ CREATE TABLE `grade` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of grade
 -- ----------------------------
 INSERT INTO `grade` VALUES ('1', '18HCB');
 INSERT INTO `grade` VALUES ('2', '17HCB');
-INSERT INTO `grade` VALUES ('3', '18HCB');
-INSERT INTO `grade` VALUES ('4', '18HCB');
-INSERT INTO `grade` VALUES ('5', '18HCB');
-INSERT INTO `grade` VALUES ('6', '18HCB');
+INSERT INTO `grade` VALUES ('7', '18HCB');
 
 -- ----------------------------
 -- Table structure for point
@@ -74,18 +71,20 @@ CREATE TABLE `point` (
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
-  `MSSV` varchar(10) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `MSSV` varchar(10) DEFAULT NULL,
   `gender` varchar(5) DEFAULT NULL,
   `CMND` varchar(15) DEFAULT NULL,
   `gradeId` varchar(10) DEFAULT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
+INSERT INTO `student` VALUES ('1', 'a', 'b', 'c', 'd', '17HCB');
+INSERT INTO `student` VALUES ('2', 'a', 'b', 'c', 'd', '17HCB');
 
 -- ----------------------------
 -- Table structure for subject
@@ -110,11 +109,10 @@ CREATE TABLE `user` (
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `role` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '1', '1', '0');
